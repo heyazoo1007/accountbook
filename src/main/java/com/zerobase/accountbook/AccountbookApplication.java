1a255f0c37733fab14c2c0fa4ae4335d5720e691
heyazoo1007 <heyazoo1007@gmail.com>
2023-01-05 12:31:48 +0900

    first commit

---
+++ src/main/java/com/zerobase/accountbook/AccountbookApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.accountbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountbookApplication.class, args);
+	}
+
+}
