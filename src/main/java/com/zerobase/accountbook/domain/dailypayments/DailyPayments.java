f91be0759c883af05517ff293a4c03bd71c8ef06
heyazoo1007 <heyazoo1007@gmail.com>
2023-01-08 16:46:37 +0900

    [feat] Dailypayments 도메인 설정

---
+++ src/main/java/com/zerobase/accountbook/domain/dailypayments/DailyPayments.java
@@ -0,0 +1,42 @@
+package com.zerobase.accountbook.domain.dailypayments;
+
+import com.zerobase.accountbook.domain.member.Member;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
+@Entity
+public class DailyPayments {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    private Long paidAmount;
+
+    private String paidWhere;
+
+    private String methodOfPayment;
+
+    private String categoryName;
+
+    private String hashTag;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
