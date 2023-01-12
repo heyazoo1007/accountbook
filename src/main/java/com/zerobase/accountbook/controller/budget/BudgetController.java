a4f1ce0ec65c6772618a2f6a9693eac32ec49b10
heyazoo1007 <heyazoo1007@gmail.com>
2023-01-12 14:59:06 +0900

    [feat] 예산 수정 기능 추가

--- src/main/java/com/zerobase/accountbook/controller/budget/BudgetController.java
+++ src/main/java/com/zerobase/accountbook/controller/budget/BudgetController.java
@@ -2,13 +2,12 @@ package com.zerobase.accountbook.controller.budget;
 
 import com.zerobase.accountbook.common.dto.ApiResponse;
 import com.zerobase.accountbook.controller.budget.dto.request.CreateBudgetRequestDto;
+import com.zerobase.accountbook.controller.budget.dto.request.ModifyBudgetRequestDto;
 import com.zerobase.accountbook.controller.budget.dto.response.CreateBudgetResponseDto;
+import com.zerobase.accountbook.controller.budget.dto.response.ModifyBudgetResponseDto;
 import com.zerobase.accountbook.service.budget.BudgetService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -25,6 +24,15 @@ public class BudgetController {
     ) {
         CreateBudgetResponseDto response =
                 budgetService.createBudget(request);
+        return ApiResponse.success(response);
+    }
+
+    @PutMapping()
+    public ApiResponse<ModifyBudgetResponseDto> modifyBudget(
+            @Valid @RequestBody ModifyBudgetRequestDto request
+    ) {
+        ModifyBudgetResponseDto response =
+                budgetService.modifyBudget(request);
         return ApiResponse.success(response);
     }
 }
--- src/main/java/com/zerobase/accountbook/service/budget/BudgetService.java
+++ src/main/java/com/zerobase/accountbook/service/budget/BudgetService.java
@@ -1,9 +1,10 @@
 package com.zerobase.accountbook.service.budget;
 
-import com.zerobase.accountbook.common.exception.ErrorCode;
 import com.zerobase.accountbook.common.exception.model.AccountBookException;
 import com.zerobase.accountbook.controller.budget.dto.request.CreateBudgetRequestDto;
+import com.zerobase.accountbook.controller.budget.dto.request.ModifyBudgetRequestDto;
 import com.zerobase.accountbook.controller.budget.dto.response.CreateBudgetResponseDto;
+import com.zerobase.accountbook.controller.budget.dto.response.ModifyBudgetResponseDto;
 import com.zerobase.accountbook.domain.budget.Budget;
 import com.zerobase.accountbook.domain.budget.BudgetRepository;
 import com.zerobase.accountbook.domain.member.Member;
@@ -15,6 +16,8 @@ import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.util.Optional;
 
+import static com.zerobase.accountbook.common.exception.ErrorCode.*;
+import static com.zerobase.accountbook.common.exception.ErrorCode.FORBIDDEN_EXCEPTION;
 import static com.zerobase.accountbook.common.exception.ErrorCode.NOT_FOUND_USER_EXCEPTION;
 
 @Service
@@ -31,9 +34,13 @@ public class BudgetService {
         Member member = validateMember(request.getMemberEmail());
 
         // 이미 생성된 예산에 대해서는 생성할 수 없고, 따로 수정 버튼을 눌러야 합니다.
-        Optional<Budget> optionalBudget = budgetRepository.findByBudgetYearMonth(YearMonth.now().toString());
+        Optional<Budget> optionalBudget =
+                budgetRepository.findByBudgetYearMonth(YearMonth.now().toString());
         if (optionalBudget.isPresent()) {
-            throw new AccountBookException("이미 해당 달에 예산이 설정 되어 있습니다.", ErrorCode.CONFLICT_MONTHLY_BUDGET_EXCEPTION);
+            throw new AccountBookException(
+                    "이미 해당 달에 예산이 설정 되어 있습니다.",
+                    CONFLICT_MONTHLY_BUDGET_EXCEPTION
+            );
         }
 
         member.setMonthlyBudget(request.getMonthlyBudget());
@@ -49,10 +56,50 @@ public class BudgetService {
                 ));
     }
 
+    public ModifyBudgetResponseDto modifyBudget(ModifyBudgetRequestDto request) {
+
+        Member member = validateMember(request.getMemberEmail());
+
+        Budget budget = validateBudget(request.getYearMonth());
+
+        requestMemberMismatchBudgetOwner(member, budget);
+
+        budget.setMonthlyBudget(request.getModifyMonthlyBudget());
+
+        return ModifyBudgetResponseDto.of(budgetRepository.save(budget));
+    }
+
+    private static void requestMemberMismatchBudgetOwner(
+            Member member, Budget budget
+    ) {
+        Member budgetOwner = budget.getMember();
+        if (!budgetOwner.equals(member)) {
+            throw new AccountBookException(
+                    "접근할 수 없는 예산입니다.",
+                    FORBIDDEN_EXCEPTION
+            );
+        }
+    }
+
+    private Budget validateBudget(String yearMonth) {
+        Optional<Budget> optionalBudget =
+                budgetRepository.findByBudgetYearMonth(yearMonth);
+        if (!optionalBudget.isPresent()) {
+            throw new AccountBookException(
+                    "존재하지 않는 예산입니다.",
+                    NOT_FOUND_BUDGET_EXCEPTION
+            );
+        }
+        return optionalBudget.get();
+    }
+
     private Member validateMember(String memberEmail) {
         Optional<Member> optionalMember = memberRepository.findByEmail(memberEmail);
         if (!optionalMember.isPresent()) {
-            throw new AccountBookException("존재하지 않는 회원입니다.", NOT_FOUND_USER_EXCEPTION);
+            throw new AccountBookException(
+                    "존재하지 않는 회원입니다.",
+                    NOT_FOUND_USER_EXCEPTION
+            );
         }
         return optionalMember.get();
     }
