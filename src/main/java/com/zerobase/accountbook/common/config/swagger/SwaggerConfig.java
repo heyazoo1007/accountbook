b50e68fe3606c789481d1e244925cc5372d48583
heyazoo1007 <heyazoo1007@gmail.com>
2023-01-22 23:46:54 +0900

    [feat] Swagger 적용

---
+++ src/main/java/com/zerobase/accountbook/common/config/swagger/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.zerobase.accountbook.common.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("zerobase.accountbook"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("김예진 개인프로젝트 - 짠짠이 입니다 :)")
+                .description("오늘 하루 소비한 내역을 저장해 가계부를 작성할 수 있어요.")
+                .version("2.0")
+                .build();
+    }
+}
