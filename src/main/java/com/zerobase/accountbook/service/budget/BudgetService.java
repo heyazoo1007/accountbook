4b61b0150c56ec17e7385cf622ef9b2b5f661e19
heyazoo1007 <heyazoo1007@gmail.com>
2024-03-02 17:56:07 +0900

    2024-03-02 fix : 자잘한 거 수정

--- src/main/java/com/zerobase/accountbook/service/budget/BudgetService.java
+++ src/main/java/com/zerobase/accountbook/service/budget/BudgetService.java
@@ -11,16 +11,14 @@ import com.zerobase.accountbook.domain.budget.BudgetRepository;
 import com.zerobase.accountbook.domain.dailypayments.DailyPaymentsRepository;
 import com.zerobase.accountbook.domain.member.Member;
 import com.zerobase.accountbook.domain.member.MemberRepository;
-import com.zerobase.accountbook.service.firebase.FirebaseCloudMessageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
+import java.util.List;
 import java.util.Optional;
 
 import static com.zerobase.accountbook.common.exception.ErrorCode.*;
@@ -32,14 +30,9 @@ import static com.zerobase.accountbook.common.exception.ErrorCode.NOT_FOUND_USER
 public class BudgetService {
 
     private final BudgetRepository budgetRepository;
-
     private final MemberRepository memberRepository;
-
     private final DailyPaymentsRepository dailyPaymentsRepository;
 
-    private final FirebaseCloudMessageService firebaseCloudMessageService;
-
-    // TODO 매달 첫번째 날에 사용자가 로그인 하면 생성하라고 알람 보내기
     public CreateBudgetResponseDto createBudget(
             String memberEmail, CreateBudgetRequestDto request
     ) {
@@ -94,73 +87,16 @@ public class BudgetService {
         return GetBudgetResponseDto.of(budget);
     }
 
-    @Scheduled(cron = "0 0 9 * * * *") // 매일 아침 9시에 예산 알림 전송
-    public void sendBudgetAlarm() throws IOException {
-        LocalDateTime now = LocalDateTime.now();
-        String budgetYearMonth = now.getYear() + "-" + now.getMonthValue();
-
-        int totalMember = Math.toIntExact(memberRepository.countBy());
-        for (int i = 0; i < totalMember / 100 + 1; i++) {
-            Page<Member> members = memberRepository.findAll(
-                    PageRequest.of(i, 100)
-            );
-            for (Member each : members) {
-
-                // budgetService 에서 한 달 예산 가져오기
-                int monthlyBudget =
-                        getMonthlyBudget(budgetYearMonth, each.getEmail());
-                // dailyPaymentsService 에서 그 때까지 쓴 돈 가져오기
-                int totalAmountSoFar =
-                        getTotalAmountSoFar(budgetYearMonth, each.getId());
-
-                // 예산 - 총 지출 금액 남았다는 메시지 전달하면 됨
-                int diffBetweenBudgetAndPaid = monthlyBudget - totalAmountSoFar;
-                if (diffBetweenBudgetAndPaid >= 0) {
-                    sendNormalBudgetAlarm("", diffBetweenBudgetAndPaid);
-                }
-                if (diffBetweenBudgetAndPaid < 0) {
-                    sendExceedBudgetAlarm("");
-                }
-            }
-        }
-    }
-
-    private void sendExceedBudgetAlarm(String targetToken) throws IOException {
-        String message = "이번 달 예산을 초과했습니다! 예산을 다시 설정하시겠어요?";
-        firebaseCloudMessageService.sendMessageTo(
-                targetToken, "오늘의 예산", message
-        );
-    }
-
-    private void sendNormalBudgetAlarm(
-            String targetToken, int diffBetweenBudgetAndPaid
-    ) throws IOException {
-        String message =
-                "이번 달 남은 예산은 " + diffBetweenBudgetAndPaid + "원 입니다.";
-        firebaseCloudMessageService.sendMessageTo(
-                targetToken, "오늘의 예산", message
-        );
-    }
-
     private int getTotalAmountSoFar(String budgetYearMonth, Long memberId) {
-        int totalAmountSoFar =
-                dailyPaymentsRepository.totalPaidAmountSoFarByMemberId(
-                memberId, budgetYearMonth
-        );
-
-        return totalAmountSoFar;
+        return dailyPaymentsRepository.totalPaidAmountSoFarByMemberId(memberId, budgetYearMonth);
     }
 
     private int getMonthlyBudget(String budgetYearMonth, String memberEmail) {
-        int monthlyBudget =
-                getBudget(memberEmail, budgetYearMonth).getMonthlyBudget();
-
-        return monthlyBudget;
+        return getBudget(memberEmail, budgetYearMonth).getMonthlyBudget();
     }
 
     private static void checkBudgetOwner(Member member, Budget budget) {
-        Member budgetOwner = budget.getMember();
-        if (!budgetOwner.equals(member)) {
+        if (!budget.getMember().equals(member)) {
             throw new AccountBookException(
                     "접근할 수 없는 예산입니다.",
                     FORBIDDEN_EXCEPTION
@@ -169,7 +105,6 @@ public class BudgetService {
     }
 
     private Budget validateBudget(String budgetYearMonth) {
-
         return budgetRepository.findByBudgetYearMonth(budgetYearMonth)
                 .orElseThrow(() -> new AccountBookException(
                         "존재하지 않는 예산입니다.",
@@ -177,9 +112,8 @@ public class BudgetService {
     }
 
     private void checkBudgetAlreadyCreated() {
-        Optional<Budget> optionalBudget =
-                budgetRepository
-                        .findByBudgetYearMonth(YearMonth.now().toString());
+        Optional<Budget> optionalBudget = budgetRepository
+                .findByBudgetYearMonth(YearMonth.now().toString());
         if (optionalBudget.isPresent()) {
             throw new AccountBookException(
                     "이미 해당 달에 예산이 설정 되어 있습니다.",
@@ -192,8 +126,7 @@ public class BudgetService {
         return memberRepository.findByEmail(memberEmail).orElseThrow(
                 () -> new AccountBookException(
                         "존재하지 않는 회원입니다.",
-                        NOT_FOUND_USER_EXCEPTION
-                )
+                        NOT_FOUND_USER_EXCEPTION)
         );
     }
 }
