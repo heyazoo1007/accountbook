880bf5e539e03ca513fbc3d1015f0362e4773b41
heyazoo1007 <heyazoo1007@gmail.com>
2024-03-17 18:44:46 +0900

    2024-03-17 메인 화면 구현

---
+++ src/main/java/com/zerobase/accountbook/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.zerobase.accountbook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/v1")
+public class IndexController {
+
+    @GetMapping()
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/signup")
+    public String createMember() {
+        return "signup";
+    }
+
+    @GetMapping("/login")
+    public String logIn() {
+        return "login";
+    }
+}
