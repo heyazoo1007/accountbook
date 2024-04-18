4baae5fbc1a56fe1d5c8c2649189395dc161fb91
heyazoo1007 <heyazoo1007@gmail.com>
2024-04-18 18:54:07 +0900

    2024-04-18 웹소켓 메시지 로직 수정

--- src/main/java/com/zerobase/accountbook/service/budget/AlarmService.java
+++ src/main/java/com/zerobase/accountbook/service/budget/AlarmService.java
@@ -14,6 +14,7 @@ import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,26 +22,24 @@ public class AlarmService {
     private final MemberRepository memberRepository;
     private final DailyPaymentsRepository dailyPaymentsRepository;
 
-    public SendBudgetAlarmDto sendBudgetAlarm() {
+    public SendBudgetAlarmDto sendBudgetAlarm(long memberId) {
         LocalDateTime now = LocalDateTime.now();
         YearMonth yearMonth = YearMonth.of(now.getYear(), now.getMonth());
         String startDate = yearMonth + "-01";
         String endDate = yearMonth + "-" + yearMonth.lengthOfMonth();
 
-        List<Member> members = memberRepository.findAll();
-        for (Member member : members) {
-            String message;
-            Integer monthlySum = dailyPaymentsRepository.findMonthlySum(member.getId(), startDate, endDate);
-            Integer monthlyBudget = member.getMonthlyBudget();
-            if (monthlySum == null) continue; // 지출내역이 없을 수도 있어서 monthlySum == null
+        Member member = memberRepository.findById(memberId).get();
+        Integer monthlySum = dailyPaymentsRepository.findMonthlySum(member.getId(), startDate, endDate);
+        Integer monthlyBudget = member.getMonthlyBudget();
 
-            if (monthlyBudget > monthlySum) {
-                message = "남은 예산은 " + (monthlyBudget - monthlySum) + " 입니다.";
-            } else {
-                message = "이번달 예산을 초과했습니다! 예산을 다시 설정해보시겠어요?";
-            }
-            return SendBudgetAlarmDto.of(member.getId(), message);
+        String message;
+        if (monthlySum == null) {
+            message = "지출내역이 존재하지 않습니다.";
+        } else if (monthlyBudget > monthlySum) {
+            message = "남은 예산은 " + (monthlyBudget - monthlySum) + " 입니다.";
+        } else {
+            message = "이번달 예산을 초과했습니다! 예산을 다시 설정해보시겠어요?";
         }
-        return null;
+        return SendBudgetAlarmDto.of(member.getId(), message);
     }
 }
