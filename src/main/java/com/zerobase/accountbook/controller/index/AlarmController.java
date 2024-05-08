b5b727377ae20aba7fd0fecf3267ea7a12185d62
heyazoo1007 <heyazoo1007@gmail.com>
2024-05-08 16:32:15 +0900

    2024-05-08 알림 헤더 관련 코드 수정

--- src/main/java/com/zerobase/accountbook/controller/index/AlarmController.java
+++ src/main/java/com/zerobase/accountbook/controller/index/AlarmController.java
@@ -20,12 +20,6 @@ public class AlarmController {
     private final SimpMessageSendingOperations messagingTemplate; // SimpMessageSendingOperation 인터페이스가 뭐지?
     private final AlarmService alarmService;
 
-// 알림 보내는 페이지로 이동
-//    @GetMapping("/v1/budget/alarm")
-//    public String stompAlarm() {
-//        return "test";
-//    }
-
     /**
      * @DestinationVariable 은 @PathVariable 과 비슷
      * /ws-stomp 로 소켓 연결하면, 클라이언트에서 /sub/{memberId} 구독
--- src/main/java/com/zerobase/accountbook/service/budget/AlarmService.java
+++ src/main/java/com/zerobase/accountbook/service/budget/AlarmService.java
@@ -1,6 +1,8 @@
 package com.zerobase.accountbook.service.budget;
 
 import com.zerobase.accountbook.common.CommonTime;
+import com.zerobase.accountbook.common.exception.ErrorCode;
+import com.zerobase.accountbook.common.exception.model.AccountBookException;
 import com.zerobase.accountbook.controller.budget.dto.response.SendBudgetAlarmDto;
 import com.zerobase.accountbook.domain.budget.BudgetRepository;
 import com.zerobase.accountbook.domain.dailypayments.DailyPaymentsRepository;
@@ -21,18 +23,25 @@ public class AlarmService {
     public SendBudgetAlarmDto sendBudgetAlarm(long memberId) {
         LocalDateTime now = LocalDateTime.now();
 
-        Member member = memberRepository.findById(memberId).get();
+        Member member = memberRepository.findById(memberId).orElseThrow(
+                () -> new AccountBookException("존재하지 않는 회원입니다.",
+                                                ErrorCode.NOT_FOUND_USER_EXCEPTION));
+
         Integer monthlySum = dailyPaymentsRepository.
                 findMonthlySum(member.getId(), CommonTime.getStartDate(now), CommonTime.getEndDate(now));
         Integer monthlyBudget = budgetRepository.
                 findByMemberIdAndYearMonth(member.getId(), CommonTime.getYearMonthString(now));
+        String message = getAlarmMessage(monthlyBudget, monthlySum);
 
-        String message;
+        return SendBudgetAlarmDto.of(member.getId(), message);
+    }
+
+    private String getAlarmMessage(Integer monthlyBudget, Integer monthlySum) {
         if (monthlyBudget == null) {
-            message = "이번달 예산이 설정되어 있지 않습니다. 새로 설정하세요!";
-            return SendBudgetAlarmDto.of(member.getId(), message);
+            return "이번달 예산이 설정되어 있지 않습니다. 새로 설정하세요!";
         }
 
+        String message;
         if (monthlySum == null) {
             message = "지출내역이 존재하지 않습니다.";
         } else if (monthlyBudget > monthlySum) {
@@ -40,6 +49,6 @@ public class AlarmService {
         } else {
             message = "이번달 예산을 초과했습니다! 예산을 다시 설정해보시겠어요?";
         }
-        return SendBudgetAlarmDto.of(member.getId(), message);
+        return message;
     }
 }
